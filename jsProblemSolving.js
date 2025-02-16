47e8a21a69defeef5add7c4e44524e989c4d2555
mahfuz-7148 <mrahman7148@gmail.com>
2025-02-17 01:02:26 +0600

    first commit

---
+++ jsProblemSolving.js
@@ -0,0 +1,24 @@
+// Write a JavaScript function countWords  that takes a string and returns the number of words contained in this string. 
+
+// Example:
+// Input: "DEV ZeroOne is a Next-Gen Programming Learning Platform."
+// Output: 8
+
+// Constraints:
+// using built-in functions is prohibited
+
+function countWords(str) {
+    let count = 0
+    let hasWord = false
+    for (let char of str) {
+        if (char !== ' ') {
+            hasWord = true;
+        }
+        else if (hasWord) {
+            count++
+            hasWord = false
+        }
+    }
+    console.log(hasWord ? count + 1: count)
+}
+countWords("DEV ZeroOne is a Next-Gen Programming Learning Platform.")
