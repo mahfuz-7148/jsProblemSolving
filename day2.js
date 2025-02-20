1fe7d9d01a9483c3e56087191eeadfe2643c7d9e
mahfuz-7148 <mrahman7148@gmail.com>
2025-02-20 14:53:12 +0600

    day2

---
+++ day2.js
@@ -0,0 +1,29 @@
+// Write a JavaScript function countWords  that takes a string and returns the number of words contained in this string.
+
+// Example:
+// Input: "DEV ZeroOne is a Next-Gen Programming Learning Platform."
+// Output: 8
+
+// Constraints:
+// using built-in functions is prohibited
+
+function countWords(str) {
+    if (typeof str !== 'string') {
+        console.log(`invalid`)
+        return
+    }
+    let count = 0
+    let hasWord = false
+    for (let char of str) {
+        if (char !== ' ') {
+            hasWord = true;
+        }
+        else if (hasWord) {
+            count++
+            hasWord = false
+        }
+    }
+    return hasWord ? count + 1 : count
+}
+let output = countWords("DEV ZeroOne is a Next-Gen Programming Learning Platform.")
+console.log(output)
--- jsProblemSolving.js
+++ jsProblemSolving.js
@@ -1,31 +1,4 @@
-// Write a JavaScript function countWords  that takes a string and returns the number of words contained in this string.
 
-// Example:
-// Input: "DEV ZeroOne is a Next-Gen Programming Learning Platform."
-// Output: 8
-
-// Constraints:
-// using built-in functions is prohibited
-
-// function countWords(str) {
-//     if(typeof str !== 'string'){
-//         console.log(`invalid`)
-//         return
-//     }
-//     let count = 0
-//     let hasWord = false
-//     for (let char of str) {
-//         if (char !== ' ') {
-//             hasWord = true;
-//         }
-//         else if (hasWord) {
-//             count++
-//             hasWord = false
-//         }
-//     }
-//    console.log(hasWord ? count + 1: count)
-// }
-// countWords("DEV ZeroOne is a Next-Gen Programming Learning Platform.")
 
 
 // Write a JavaScript function removeWords  that takes a string as input and returns the remaining string after removing the first three characters.
